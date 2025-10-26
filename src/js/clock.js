62ece13610359edf5dcb7173d717f742f9dcf12a
Kauan Silva <kauan@gamasoft.com.br>
2025-10-26 00:45:57 -0300

    Ajustado a nomenclatura de algumas constantes

--- src/js/clock.js
+++ src/js/clock.js
@@ -3,22 +3,22 @@ function primeiraMaiuscula(string) {
 }
 
 function capturarHorarioAtual() {
-  const agora = new Date();
-  const horas = agora.getHours().toString().padStart(2, '0'); 
-  const minutos = agora.getMinutes().toString().padStart(2, '0');
-  const segundos = agora.getSeconds().toString().padStart(2, '0');
+  const horario = new Date();
+  const horas = horario.getHours().toString().padStart(2, '0'); 
+  const minutos = horario.getMinutes().toString().padStart(2, '0');
+  const segundos = horario.getSeconds().toString().padStart(2, '0');
 
   document.getElementById('currentTime').textContent = `${horas}:${minutos}:${segundos}`; 
 }
 
 function atualizarDataAtual() {
-  const agora = new Date();
-  const diaSemana = primeiraMaiuscula(agora.toLocaleDateString('pt-BR', { weekday: 'long' }));
-  const dia = agora.getDate().toString().padStart(2, '0');
-  const mes = agora.toLocaleDateString('pt-BR', { month: 'long' });
-  const ano = agora.getFullYear();
+  const hoje = new Date();
+  const semana = primeiraMaiuscula(hoje.toLocaleDateString('pt-BR', { weekday: 'long' }));
+  const dia = hoje.getDate().toString().padStart(2, '0');
+  const mes = hoje.toLocaleDateString('pt-BR', { month: 'long' });
+  const ano = hoje.getFullYear();
 
-  document.getElementById('currentDate').textContent = `${diaSemana}, ${dia} de ${mes} de ${ano}`;
+  document.getElementById('currentDate').textContent = `${semana}, ${dia} de ${mes} de ${ano}`;
 }
 function atualizarTudo() {
   capturarHorarioAtual();
