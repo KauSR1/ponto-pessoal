204bedb5604f9acda1cf24b3f1b182b86bd5208f
Kauan Silva <kauan@gamasoft.com.br>
2025-11-14 11:12:36 -0300

    Feat/ Criado o modal para digitar o que foi feito no dia

---
+++ src/js/modal.js
@@ -0,0 +1,24 @@
+const modalBtnSave = document.getElementById('btnModalSave');
+const modalBtnCancel = document.getElementById('btnModalCancel');
+const modal = document.getElementById('modalOverlay');
+
+modalBtnSave.addEventListener('click', () => {
+  const textarea = document.getElementById('modalTextarea');
+  const texto = textarea.value.trim();
+
+  if (!texto) {
+    alert("Digite algo!");
+    return;
+  }
+
+  window.arrayRegistros[window.indiceEditando].descricao = texto;
+  
+  console.log('Salvo:', window.arrayRegistros[window.indiceEditando]);
+  
+  modal.classList.remove('active');
+  textarea.value = '';
+});
+
+modalBtnCancel.addEventListener('click', () => {
+  modal.classList.remove('active');
+});
